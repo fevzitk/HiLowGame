4939392c5302cfd15e0721470917d77db42002f0
Fevzi Taha Kilic <fevzi.kilic@icloud.com>
2024-11-17 12:56:53 -0500

    programmed Hi-Lo game

--- src/main/java/org/fevzi/Main.java
+++ src/main/java/org/fevzi/Main.java
@@ -5,7 +5,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World.");
+        System.out.println("HI-LO GUESSING GAME");
+
+        Scanner scan = new Scanner(System.in);
+        Random gen = new Random(System.currentTimeMillis());
+
+        int num = 0, guess = 0;
+        boolean running = true;
+        String answer;
+
+        while (running == true) {
+            running = false;
+            num = gen.nextInt(100) + 1;
+            System.out.println("Guess a number between 1 and 100.");
+            guess = scan.nextInt();
+            while (guess != num) {
+                if (guess > num) {
+                    System.out.println("Your guess is too high, try again!");
+                    guess = scan.nextInt();
+                } else if (guess < num) {
+                    System.out.println("Your guess is too low, try again!");
+                    guess = scan.nextInt();
+                }
+            }
+            num = -1;
+            System.out.println("You guessed it!  Would you like to play again? [y/n]");
+            while (num == -1) {
+                answer = scan.nextLine();
+                if (answer.equals("y")) {
+                    num = 0;
+                    running = true;
+                }
+                if (answer.equals("n")) {
+                    num = 0;
+                    System.out.println("Thanks for playing!");
+                }
+            }
+        }
     }
 
 }
